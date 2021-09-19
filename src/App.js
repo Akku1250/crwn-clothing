329efda59db3f30cb86f6d49bd0fe07ddcdee783
MJ Cheriyan <midhuncheriyan@gmail.com>
2021-09-19 17:45:45 +0200

    Updating our app with a shop page, such as collection-prev and collection-item

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage_component';
-
-// const HatsPage = () => (
-//   <div>
-//     <h1>Hats Page</h1>
-//   </div>
-// )
+import ShopPage from './pages/shop/shop_component';
 
 function App() {
   return (
     <div >
       <Switch>
-           <Route exact path='/' component={HomePage}/>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
    
     </div>
