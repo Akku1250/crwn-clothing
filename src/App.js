04684f6f347c0a4e34d9ac7b369e02f723600654
MJ Cheriyan <midhuncheriyan@gmail.com>
2021-10-21 00:25:01 +0200

    implemented firebase utils, included ability to store authenticated user into our firesrestoreS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage_component';
 import ShopPage from './pages/shop/shop_component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up_component';
 import Header from './components/header/header_component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component{
   constructor() {
@@ -19,10 +19,21 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+       if(userAuth){
+         const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+         userRef.onSnapshot(snapShot =>{
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            })
+         });
+       }else{
+          this.setState({currentUser: userAuth});
+       }    
     })
   }
 
@@ -31,6 +42,7 @@ class App extends React.Component{
   }
 
   render(){
+    console.log(this.state);
     return (
       <div>
         <Header currentUser={this.state.currentUser}/>
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up_component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up_component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in_component';
+import SignUp from '../../components/sign-up/sign-up_component';
 import './sign-in-and-sign-up_style.scss';
 
 const SignInAndSignUpPage = () => (
     <div className = 'sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 )
 
